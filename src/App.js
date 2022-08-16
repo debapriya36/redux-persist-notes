d9ff623d216a68d01b05bc1e9f311d987dc1adcf
Debapriya Chandra <77222446+debapriya36@users.noreply.github.com>
2022-08-16 22:38:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Addtask from './Addtask';
+import Todolist from './Todolist';
+
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
-import Todolist from './Todolist';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import {PersistGate} from 'redux-persist/integration/react'
